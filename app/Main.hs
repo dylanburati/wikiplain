790b0008e49b54735ee184d1b867e5b3b474f57c
Dylan Burati <dylanburati@protonmail.com>
2022-07-27 21:01:01 -0400

    Try to fix quotes around MediaWiki table styles

--- app/Main.hs
+++ app/Main.hs
@@ -1,11 +1,14 @@
 {-# LANGUAGE DeriveDataTypeable #-}
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE ViewPatterns #-}
 module Main where
 
 import qualified Data.Conduit.Combinators as CC
 import qualified Data.Conduit.List as CL
+import Data.Char (isLetter)
 import Data.Text (Text)
 import qualified Data.Text as T
+import qualified Data.Text.IO as TIO
 import Data.Maybe (fromJust, fromMaybe)
 import Data.XML.Types
 import Text.XML.Stream.Parse (def)
@@ -27,6 +30,23 @@ type WikiPageBuilder = (Maybe Text, Maybe Text, [Text], [Name])
 -- 6: content <text>
 -- 7: content <revision> & post </text>
 -- 8: post </revision>
+
+fixMwProblems :: Text -> Text
+fixMwProblems t =
+  T.unlines $ map rebalanceQuotes (T.lines t)
+  where
+    rebalanceQuotes line
+      | T.isPrefixOf "{|" line = case balanceCheck $ T.unpack line of
+          (False,_,q,l) -> (T.take q line) <> " style=\"" <> (T.drop l line)
+          _ -> line
+      | otherwise = line
+    balanceCheck = foldl balCheck1 (True,0,0,0)
+    balCheck1 (True,p,q,l) '"' = (False,p+1,q,l)   {- begin quote -}
+    balCheck1 (False,p,q,l) '"' = (True,p+1,p+1,p) {- end quote -}
+    balCheck1 (True,p,q,l) ch
+      | (q > l) && (isLetter ch) = (True,p+1,q,p) {- letter after end quote -}
+      | otherwise = (True,p+1,q,l)
+    balCheck1 (False,p,q,l) _ = (False,p+1,q,l)
 
 setTitle :: WikiPageBuilder -> Text -> WikiPageBuilder
 setTitle (_,r,t,path) v = (Just v,r,t,path)
@@ -67,48 +87,21 @@ onContent _ bldr = bldr
 
 onEnd :: WikiPageBuilder -> (WikiPageBuilder, [WikiPage])
 onEnd (Just n,r,t,["page"]) = do
-  let parsed = WikiPage n r (T.concat $ reverse t)
-  ((Nothing,Nothing,[],["page"]), [parsed])
+  let text = T.concat (reverse t)
+  let pg = WikiPage n r (fixMwProblems text)
+  ((Nothing,Nothing,[],["page"]), [pg])
 onEnd (_,_,_,["page"]) = ((Nothing,Nothing,[],["page"]), [])
 onEnd bldr = (bldr, [])
 
-
--- getWikiPage :: CR.MonadThrow m => ConduitT Event o m (Maybe [(Text, Text)])
--- getWikiPage = do
---   let attrParser = requireAttr "title"
---   pipeline <- P.many' $
---     P.choose
---       [ P.tagIgnoreAttrs "title" $ \() -> do
---           title <- P.content
---           return ("title", title)
---       , P.tag' "redirect" attrParser $ \(redir) -> return ("redirect", redir)
---       , P.tagIgnoreAttrs "revision" 
---         ((P.many' (P.tagIgnoreAttrs "text" $ \() -> do
---             text <- P.content
---             return ("text", text)))
---          .| CC.head)
---       ]
---   pipeline .| CC.
---   usable
---   .| P.tagIgnoreAttrs "title" $ return P.content
---   .| CC.map (WikiPage . fromJust)
---   .| CC.head
-
--- contentOrEmpty :: MonadThrow m => Name n -> Name n2 -> ConduitT Event o m Text
--- contentOrEmpty name, boundName = do
---   case parsed of
---     [] -> ""
---     (c:_) -> ""
---   where parsed = P.manyIgnore
---     (P.tagIgnoreAttrs name P.content)
---     (P.tagIgnoreAttrs boundName )
-
 main :: IO ()
 main = do
-  result <- runConduit $
+  runConduit $
     CC.stdin
     .| P.parseBytes def
     .| CC.concatMapAccum onEvent (Nothing,Nothing,[],[])
     .| CC.filter (\(WikiPage _ redir _) -> T.length (fromMaybe "" redir) == 0)
-    .| CC.sinkList
-  putStrLn $ foldl (\acc curr -> acc ++ "\n" ++ (show curr)) "" result
+    .| CC.mapM_ (\(WikiPage title _ text) -> do
+        _ <- TIO.putStr title
+        _ <- putChar '\0'
+        _ <- TIO.putStr text
+        putChar '\0')
